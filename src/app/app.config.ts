a3b7d35a6a3ce7e28004d63ede112593b01c77b5
412017-PILLADO <412017@tecnicatura.frc.utn.edu.ar>
2026-05-06 01:00:46 -0300

    config

---
+++ src/app/app.config.ts
@@ -0,0 +1,26 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideHttpClient(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          cssLayer: {
+            name: 'primeng',
+            order: 'tailwind, primeng',
+          }
+        }
+      }
+    })
+  ]
+};
---
+++ src/app/app.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Button } from 'primeng/button';
+
+@Component({
+  selector: 'app-root',
+  imports: [Button],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {}
